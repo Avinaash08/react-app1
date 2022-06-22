cfe173eea669f484924fe50c2ac3851a3712b8a9
Avinaash08 <38736401+Avinaash08@users.noreply.github.com>
2022-06-22 17:23:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Widget from './component/widgetcomponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       <Widget name="Hi Indium"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
